4f86555181ca2acebbb1f3d3d372f344fca6aaba
Luis Martinez <rickymtz1999@gmail.com>
2023-03-13 16:32:45 -0500

    adds assignments

--- homework/class homework practice/week 8/dog ceo 4/dogceo4.js
+++ homework/class homework practice/week 8/dog ceo 4/dogceo4.js
@@ -1,8 +1,9 @@
 // const random = document.getElementById('random-dog');
 const button = document.getElementsByTagName('button')[0];
 const output = document.getElementById('output');
+const thePrompt = document.getElementById('the-prompt')
 button.addEventListener('click', handleCLick);
-// output.addEventListener('click', handleSelection);
+output.addEventListener('click', handleSelection);
 url ='https://dog.ceo/api/breeds/image/random'
 
 
@@ -10,34 +11,40 @@ function handleCLick(e) {
     addImage();
 };
 
-// function handleSelection(e) {
-//     const breed = e.target.getAttribute('data-breed');
-//     if (breed) {
-//         console.log('hit', breed === answer);
-//     }
-// }
+function handleSelection(e) {
+    const breed = e.target.getAttribute('data-breed');
+    if (breed) {
+        giveFeedback(breed === answer);
+    }
+}
 
-// function randomAnswer() {
-//     answer = breeds[Math.floor(Math.random() * breeds.length)];
-//     const prompt = document.createElement('div');
-//     prompt.innerText = `Which image shows the ${answer} breed?`;
-//     thePrompt.append(prompt);
+function randomAnswer() {
+    answer = breeds[Math.floor(Math.random() * breeds.length)];
+    const prompt = document.createElement('div');
+    prompt.innerText = `Which image shows the ${answer} breed?`;
+    thePrompt.append(prompt);
 
-// }
+}
+function giveFeedback(isCorrect) {
+    const feedback =document.createElement('div');
+    const message = isCorrect ? 'Correct' : 'Inncorret';
+    feedback.innerText = `That is ${message}!`;
+    thePrompt.append(feedback);
+}
+
+let breeds = [];
+let answer = ''
 
 async function grabDog() {
     const request = await fetch('https://dog.ceo/api/breeds/image/random');
     const response = await request.json();
-    const data = response.message;
+    const url = response.message;
     console.log(request, 'test');
-    return data;
-    // const breed = url.split('/')[4];
-    // breeds.push(breed);
-    // return {url, breed};
+    // return data;
+    const breed = url.split('/')[4];
+    breeds.push(breed);
+    return {url, breed};
 };
-
-let breeds = [];
-let answer = ''
 
 async function addImage() {
     output.innerHTML= '';
@@ -45,12 +52,13 @@ async function addImage() {
         const div = document.createElement('div');
         div.className = 'nimg';
         const img = document.createElement('img');
-        const url = await grabDog();
+        const {url, breed} = await grabDog();
         img.src = url;
-        // img.setAttribute('data-breed', breed);
+        img.setAttribute('data-breed', breed);
         div.append(img);
         output.append(div);
     }
+    randomAnswer();
 }
 
 // function removeCurrentImg() {
